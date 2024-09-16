d0868e88668b0ae223c0914cf98c37bba55378ed
aarniman <122281928+aarniman@users.noreply.github.com>
2024-09-16 15:24:33 +0300

    aaaa

---
+++ JesnkinsTest/src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    App app = new App();
+
+    @Test
+    public void testAddMe(){
+        assertEquals(5, app.addMe(2, 3));
+    }
+
+    @Test
+    public void testSubMe(){
+        assertEquals(1, app.subMe(3, 2));
+    }
+}
